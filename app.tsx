cc9a68a35940e224fb11cc08e559accb13ebbe7f
해명 <hmyeonge@gmail.com>
2025-06-14 20:13:20 +0900

    2. Introducing Re-render for our App 10

--- app.tsx
+++ app.tsx
@@ -1,5 +1,4 @@
 
-
 // -- Library ---
 // 1. React.createElement 정의 : JSX 를 분석해서 가상돔을 만듬
 const React = {
@@ -18,44 +17,64 @@ const React = {
     }
 }; 
 
+// 직접 useState 를 정의
+const useState = (initialState) => {
+    console.log('useState is initialized with value:', initialState); 
+    let state = initialState; // 인자로 받은 값이 state 
+    const setState = (newState) => {
+        console.log('setState is called with newState value:', newState); 
+        state = newState; 
+
+        // Render the UI fresh given state has changed.
+        reRender(); 
+    }
+    // 그리고 반환
+    return [state, setState]; 
+}
+
+// 리렌더링
+const reRender = () => {
+    console.log('reRender-ing :)'); 
+    const rootNode = document.getElementById('myapp');
+    rootNode.innerHTML = '';
+    //  simply calls the render fresh for the App
+    render(<App />, document.getElementById('myapp')); 
+}
+
 // el : App 함수가 리턴하는 가상돔 트리 객체
-// render 함수 목적. 가상DOM 객체(el) 을 받아서 실제 DOM 요소를 만들고, 컨테이너 안에 붙이기
 const render = (el, container) => {
-    console.log(el); // 가상돔 트리 객체
-    console.log(container); // <div id="myapp"></div>
 
     // 1. DOM 요소 만들기
-    // el.tag = 'div' > domEl = <div></div>
     let domEl = document.createElement(el.tag); 
 
     // 2. 속성 (props) 설정하기 (el.props)
-    // 우리가 만든 가상돔에 props 가 있으면 실제 DOM 요소에 그 속성을 그대로 복사하기
-    // 예) {draggable : true} 
     let elProps = el.props ? Object.keys(el.props) : null; 
     if (elProps && elProps.length > 0){
         elProps.forEach((prop) => (domEl[prop] = el.props[prop]))
     }
 
     // 3. 자식을 재귀적으로 처리 (el.children)
-    // 예) <div> 안에 <h2>, <p> 가 있는 경우 
-    // el.children 이 있으면, 자식 요소도 render( ) 를 호출해서 처리
     if(el.children && el.children.length >0){
         el.children.forEach((node) => render(node, domEl)); 
     }
 
     // 4. 만든 실제 DOM 을 부모 (container) 에 붙이기
-    // 여기가 DOM 이 브라우저에 실제로 보여지는 순간 
     container.appendChild(domEl); 
 }
 
 // ---- Application --- //
 const App = () => {
-    const myName = 'Arindam';
+    const [name, setName] = useState('Arindam'); 
     return (
     <div draggable>
-        <h2>Hello {myName}!</h2>
+        <h2>Hello {name}!</h2>
         <p>I am a paragraph</p>
-        <input type="text" />
-        </div> );
+        <input
+        type="text"
+        value={name}
+        onchange={(e) => setName(e.target.value)}
+        />
+    </div>
+    );
 };
 render(<App />, document.getElementById('myapp'));
