8b0af26bd37b6bf5c2338582650e5b218f1f7225
해명 <hmyeonge@gmail.com>
2025-06-28 20:37:01 +0900

    feat: ReactDOM.hydrate()로 SSR 페이지 하이드레이션 구현

--- src/server.tsx
+++ src/server.tsx
@@ -1,19 +1,25 @@
 // server.tsx
 import express from 'express';
 import React from 'react';
-// import { renderToString } from 'react-dom/server';
 import render from './index.server';
-const app = express();
+import {readFileSync} from 'fs'; // 추가
 
-app.get('/*', (req: any, res: any) => {  // 둘 다 any로 통일
+const app = express();
+const templateFile = './build/index.html';
+const templateHTML = readFileSync(templateFile, 'utf-8');
+
+/**
+ * 역할 : index.html 없이 빌드 폴더를 제공
+ * 빌드된 index.html > templateHTML 로 읽고 > 
+ * 제공 전 templateHTML.replace("{{APP}}",reactApp); 으로 
+ * {{App}} 를 실제 SSR 생성 컨텐츠로 교체
+ */
+app.use(express.static('./build', { index: false }));
+
+app.get('/*', (req : any, res: any) => {
   const reactApp = render(req.url);
-  return res.send(
-    `<html>
-      <body>
-        <div id="root햐"> ${reactApp}</div>
-      </body>
-    </html>`  
-  );
+  const response = templateHTML.replace("{{APP}}",reactApp);
+  return res.send(response);
 });
 
 app.listen(3000, () => {
